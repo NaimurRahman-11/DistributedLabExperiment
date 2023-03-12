e0afef95f364830950541b361b91484920fef8cb
Naimur Rahman <nirzon.naim@gmail.com>
2023-03-12 03:20:39 -0400

    Bug Fixedclear

--- OneToMany/server.java
+++ OneToMany/server.java
@@ -1,83 +1,62 @@
-package OneTOMany;
+package OneToMany;
 
-import java.util.*;
 import java.net.*;
 import java.io.*;
 
+public class server {
 
-public class server
-{
-
-    public static void main(String s[]) throws Exception
-    {
-        Socket sa=null;
-        ServerSocket ss2=null;
+    public static void main(String s[]) throws Exception {
+        Socket sa = null;
+        ServerSocket ss2 = null;
         System.out.println("server listening ");
-        try
-        {
-            ss2=new ServerSocket(4445);
-        }
-        catch(IOException e)
-        {
+        try {
+            ss2 = new ServerSocket(4445);
+        } catch (IOException e) {
             System.out.println("server error");
         }
-        while(true)
-        {
-            try
-            {
-                sa=ss2.accept();
+        while (true) {
+            try {
+                sa = ss2.accept();
                 System.out.println("connetion established");
-                ServerThread st =new ServerThread(sa);
+                ServerThread st = new ServerThread(sa);
                 st.start();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println("connetion error");
             }
         }
     }
 }
-class ServerThread extends Thread
-{
-    String line=null;
-    DataInputStream is =null;
-    PrintWriter od=null;
-    Socket s1=null;
-    public ServerThread(Socket s)
-    {
-        s1=s;
+
+class ServerThread extends Thread {
+    String line = null;
+    DataInputStream is = null;
+    PrintWriter od = null;
+    Socket s1 = null;
+
+    public ServerThread(Socket s) {
+        s1 = s;
     }
-    public void run()
-    {
-        try
-        {
 
-
-
+    public void run() {
+        try {
 
             is = new DataInputStream(s1.getInputStream());
             od = new PrintWriter(s1.getOutputStream());
 
+            line = is.readLine();
 
-            line=is.readLine();
-
-
-            while(!line.equals("QUIT"))
-            {
+            while (!line.equals("QUIT")) {
                 od.println(line);
                 od.flush();
-                System.out.println("response to client "+line);
-                line=is.readLine();
-
+                System.out.println("response to client " + line);
+                line = is.readLine();
 
             }
             is.close();
             od.close();
             s1.close();
 
-        }
-        catch(IOException ie)
-        {
+        } catch (IOException ie) {
             System.out.println("socket close error");
         }
     }
